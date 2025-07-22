16cf78b6875e7e44726436ca3509a3bd3a94e88d
Memorix101 <Memorix101@gmail.com>
2025-07-22 18:56:13 +0200

    simplify function

--- STM32_LCD/src/lcd.h
+++ STM32_LCD/src/lcd.h
@@ -14,13 +14,20 @@
 #define D7_Pin GPIO_PIN_5
 #define D7_GPIO_Port GPIOB
 
-void LCD_SendCommand(uint8_t command);
-void LCD_SendData(uint8_t data);
+typedef enum
+{
+    LCD_DATA,
+    LCD_COMMAND
+} LCD_ARG_t;
+
+// void LCD_SendCommand(uint8_t command);
+// void LCD_SendData(uint8_t data);
+void LCD_Send(uint8_t data, LCD_ARG_t arg);
 void LCD_Clear(void);
 void LCD_WriteString(char *str);
 void LCD_SetCursor(uint8_t row, uint8_t col);
 void LCD_GotoXY(uint8_t x, uint8_t y);
 void LCD_Home(void);
 void LCD_WriteChar(char c);
-void LCD_WriteStringClear(char* str);
-void LCD_WriteStringAtPosition(uint8_t row, uint8_t col, char* str, uint8_t fieldWidth);
+void LCD_WriteStringClear(char *str);
+void LCD_WriteStringAtPosition(uint8_t row, uint8_t col, char *str, uint8_t fieldWidth);
